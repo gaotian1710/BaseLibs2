81d3c8799d3435d5c0002c6864c64d422b5e86d8
Yoshinao Takada <yoshinao_takada2002@yahoo.co.jp>
2018-04-07 13:03:37 +0900

    intermediate commit. BLBlas might be fail to design well?

--- BaseLibs2/BLBlas.c
+++ BaseLibs2/BLBlas.c
@@ -76,4 +76,95 @@ void BLBlas_ZpAddEqXpF(PBLPtr z, PBLCPtr x)
 	*(z->f++) += *(x->f++);
 }
 
+inline void BLBlas_ZpSubEqXpF(PBLPtr z, PBLCPtr x)
+{
+	*(z->f++) -= *(x->f++);
+}
+
+inline void BLBlas_ZpMulEqXpF(PBLPtr z, PBLCPtr x)
+{
+	*(z->f++) *= *(x->f++);
+}
+
+inline void BLBlas_ZpDivEqXpF(PBLPtr z, PBLCPtr x)
+{
+	*(z->f++) /= *(x->f++);
+}
+
+inline void BLBlas_ZpEqNegativeXpF(PBLPtr z, PBLCPtr x)
+{
+	*(z->f++) = (-1.0f) * *(x->f++);
+}
+
+inline void BLBlas_ZpEqReciprocalXpF(PBLPtr z, PBLCPtr x)
+{
+	*(z->f++) = 1.0 / (*(x->f++));
+}
+
+inline void BLBlas_ZpEqXpD(PBLPtr z, PBLCPtr x)
+{
+	*(z->d++) = *(x->d++);
+}
+
+inline void BLBlas_ZpAddEqXpD(PBLPtr z, PBLCPtr x)
+{
+	*(z->d++) += *(x->d++);
+}
+
+inline void BLBlas_ZpSubEqXpD(PBLPtr z, PBLCPtr x)
+{
+	*(z->d++) -= *(x->d++);
+}
+
+inline void BLBlas_ZpMulEqXpD(PBLPtr z, PBLCPtr x)
+{
+	*(z->d++) *= *(x->d++);
+}
+
+inline void BLBlas_ZpDivEqXpD(PBLPtr z, PBLCPtr x)
+{
+	*(z->d++) /= *(x->d++);
+}
+
+inline void BLBlas_ZpEqNegativeXpD(PBLPtr z, PBLCPtr x)
+{
+	*(z->d++) = (-1.0) * (*(x->d++));
+}
+
+inline void BLBlas_ZpEqReciprocalXpD(PBLPtr z, PBLCPtr x)
+{
+	*(z->d++) = 1.0/ (*(x->d++));
+}
+
+inline void BLBlas_ZpEqXpFc(PBLPtr z, PBLCPtr x)
+{
+	*(z->fc++) = *(x->fc++);
+}
+
+inline void BLBlas_ZpAddEqXpFc(PBLPtr z, PBLCPtr x)
+{
+	float* zf = z->f;
+	const float* xf = x->f;
+	*zf++ += *xf++;
+	*zf++ += *xf++;
+	z->fc = (_Fcomplex*)zf;
+	x->fc = (_Fcomplex*)xf;
+}
+
+inline void BLBlas_ZpSubEqXpFc(PBLPtr z, PBLCPtr x)
+{
+	float* zf = z->f;
+	const float* xf = x->f;
+	*zf++ -= *xf++;
+	*zf++ -= *xf++;
+	z->fc = (_Fcomplex*)zf;
+	x->fc = (_Fcomplex*)xf;
+}
+
+inline void BLBlas_ZpMulEqXpFc(PBLPtr z, PBLCPtr x)
+{
+	*(z->fc) = _FCmulcc(*(z->fc), *(x->fc++));
+	z->fc++;
+}
+
 #pragma endregion _1v1c operations
--- BaseLibs2/BLBlas.h
+++ BaseLibs2/BLBlas.h
@@ -11,60 +11,60 @@ extern "C" {
 	// Dc: double complex
 
 	// 1v0c operations
-	void BLBlas_ZpEqZeroF(PBLPtr z);
-	void BLBlas_ZpEqOneF(PBLPtr z);
-	void BLBlas_ZpEqNegativeOneF(PBLPtr z);
-	void BLBlas_ZpEqZeroD(PBLPtr z);
-	void BLBlas_ZpEqOneD(PBLPtr z);
-	void BLBlas_ZpEqNegativeOneD(PBLPtr z);
-	void BLBlas_ZpEqZeroFc(PBLPtr z);
-	void BLBlas_ZpEqOneFc(PBLPtr z);
-	void BLBlas_ZpEqNegativeOneFc(PBLPtr z);
-	void BLBlas_ZpEqZeroDc(PBLPtr z);
-	void BLBlas_ZpEqOneDc(PBLPtr z);
-	void BLBlas_ZpEqNegativeOneDc(PBLPtr z);
+	inline void BLBlas_ZpEqZeroF(PBLPtr z);			/*!< *z++ = (float)0.0 */
+	inline void BLBlas_ZpEqOneF(PBLPtr z);			/*!< *z++ = (float)1.0 */
+	inline void BLBlas_ZpEqNegativeOneF(PBLPtr z);	/*! *z++ = (float)(-1.0) */
+	inline void BLBlas_ZpEqZeroD(PBLPtr z);			/*! *z++ = (double)0.0 */
+	inline void BLBlas_ZpEqOneD(PBLPtr z);			/*! *z++ = (double)1.0 */
+	inline void BLBlas_ZpEqNegativeOneD(PBLPtr z);	/*! *z++ = (double)(-1.0) */
+	inline void BLBlas_ZpEqZeroFc(PBLPtr z);		/*! *z++ = _FCOMPLEX_(0.0, 0.0) */
+	inline void BLBlas_ZpEqOneFc(PBLPtr z);			/*! *z++ = _FCOMPLEX_(1.0, 0.0) */
+	inline void BLBlas_ZpEqNegativeOneFc(PBLPtr z);	/*! *z++ = _FCOMPLEX_(-1.0, 0.0) */
+	inline void BLBlas_ZpEqZeroDc(PBLPtr z);		/*! *z++ = _DCOMPLEX_(0.0, 0.0) */
+	inline void BLBlas_ZpEqOneDc(PBLPtr z);			/*! *z++ = _DCOMPLEX_(1.0, 0.0) */
+	inline void BLBlas_ZpEqNegativeOneDc(PBLPtr z);	/*! *z++ = _DCOMPLEX_(-1.0, 0.0) */
 
 	// 1v1c operations
-	void BLBlas_ZpEqXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpAddEqXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpSubEqXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpMulEqXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpDivEqXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqNegativeXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqReciprocalXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpAddEqXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpSubEqXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpMulEqXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpDivEqXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqNegativeXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqReciprocalXpD(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqXpF(PBLPtr z, PBLCPtr x);			/*!< *z++ = *x++ */
+	inline void BLBlas_ZpAddEqXpF(PBLPtr z, PBLCPtr x);			/*!< *z++ += *x++ */
+	inline void BLBlas_ZpSubEqXpF(PBLPtr z, PBLCPtr x);			/*!< *z++ -= *x++ */
+	inline void BLBlas_ZpMulEqXpF(PBLPtr z, PBLCPtr x);			/*!< *z++ *= *x++ */
+	inline void BLBlas_ZpDivEqXpF(PBLPtr z, PBLCPtr x);			/*!< *z++ /= *x++ */
+	inline void BLBlas_ZpEqNegativeXpF(PBLPtr z, PBLCPtr x);	/*!< *z++ = (-1.0) * *x++ */
+	inline void BLBlas_ZpEqReciprocalXpF(PBLPtr z, PBLCPtr x);	/*!< *z++ = 1.0/*x++ */
+	inline void BLBlas_ZpEqXpD(PBLPtr z, PBLCPtr x);			/*!< *z++ = *x++ */
+	inline void BLBlas_ZpAddEqXpD(PBLPtr z, PBLCPtr x);			/*!< *z++ *= *x++ */
+	inline void BLBlas_ZpSubEqXpD(PBLPtr z, PBLCPtr x);			/*!< *z++ -= *x++ */
+	inline void BLBlas_ZpMulEqXpD(PBLPtr z, PBLCPtr x);			/*!< *z++ *= *x++ */
+	inline void BLBlas_ZpDivEqXpD(PBLPtr z, PBLCPtr x);			/*!< *z++ /= *x++ */
+	inline void BLBlas_ZpEqNegativeXpD(PBLPtr z, PBLCPtr x);	/*!< *z++ = (-1.0) * *x++ */
+	inline void BLBlas_ZpEqReciprocalXpD(PBLPtr z, PBLCPtr x);	/*!< *z++ = 1.0/(*x++) */
 
-	void BLBlas_ZpEqXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpAddEqXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpSubEqXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpMulEqXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpDivEqXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqNegativeXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqReciprocalXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqConjugateXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpAddEqXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpSubEqXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpMulEqXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpDivEqXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqNegativeXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqReciprocalXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpEqConjugateXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpAddEqXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpSubEqXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpMulEqXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpDivEqXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqNegativeXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqReciprocalXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqConjugateXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpAddEqXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpSubEqXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpMulEqXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpDivEqXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqNegativeXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqReciprocalXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZpEqConjugateXpDc(PBLPtr z, PBLCPtr x);
 
-	void BLBlas_ZpfEqAddEqXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpfEqMulEqXpF(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpfEqAddEqXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpfEqMulEqXpD(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpfEqAddEqXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpfEqMulEqXpFc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpfEqAddEqXpDc(PBLPtr z, PBLCPtr x);
-	void BLBlas_ZpfEqMulEqXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqAddEqXpF(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqMulEqXpF(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqAddEqXpD(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqMulEqXpD(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqAddEqXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqMulEqXpFc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqAddEqXpDc(PBLPtr z, PBLCPtr x);
+	inline void BLBlas_ZfEqMulEqXpDc(PBLPtr z, PBLCPtr x);
 #if defined(__cplusplus)
 }
 #endif
